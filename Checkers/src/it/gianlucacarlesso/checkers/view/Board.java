f2fffa3e966de441710cb9e120d8951d9d339bcf
Gianluca Carlesso <info@gianlucacarlesso.it>
2013-05-21 21:04:18 +0200

    Completed arrangement of the pieces on the Damiera

--- Checkers/src/it/gianlucacarlesso/checkers/view/Board.java
+++ Checkers/src/it/gianlucacarlesso/checkers/view/Board.java
@@ -2,7 +2,6 @@ package it.gianlucacarlesso.checkers.view;
 
 import it.gianlucacarlesso.checkers.R;
 import it.gianlucacarlesso.checkers.utilities.DisplayProperties;
-import android.R.integer;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
@@ -10,21 +9,23 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Point;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
 public class Board extends View {
+	private static int NUM_BOX_ROW = 8;
+	private static int PLAYER_ROWS = 3;
+	private static Point SIZE_BOARD_ORIGIN = new Point(1319, 1406);
+	private static Point CENTER_BOARD_ORIGIN = new Point(649, 627);
+	private static Point SIZE_BOX_ORIGIN = new Point(140, 140);
+
 	private Context context;
 	private Bitmap board;
 	private Bitmap piece_black;
 	private Bitmap piece_white;
 	private int shadow = 17;
 	private Point screen_size;
-	private static int NUM_BOX_ROW = 8;
-	private static Point SIZE_BOARD_ORIGIN = new Point(1319, 1406);
-	private static Point CENTER_BOARD_ORIGIN = new Point(649, 627);
-	private static Point SIZE_BOX_ORIGIN = new Point(140, 140);
+	private Point[][] matrix = new Point[NUM_BOX_ROW][NUM_BOX_ROW];
 
 	int box_size_x;
 	int box_size_y;
@@ -63,10 +64,36 @@ public class Board extends View {
 		canvas.drawBitmap(board, pos_x, pos_y, null);
 
 		// Draw pieces
-		int correct = (int) ((box_size_x - piece_black.getWidth()) / 2.0);
-		Log.i("XXX", "CO: " + correct +"--" + "b:" + box_size_x + "---" + piece_black.getWidth());
-		canvas.drawBitmap(piece_black, board_center_x - box_size_x - correct,
-				board_center_y - box_size_y - correct, null);
+		int correct = (int) ((box_size_x + pos_x - piece_black.getWidth()) / 2.0);
+
+		// Black pieces
+		int rows = 0;
+		for (int i = 0; i < PLAYER_ROWS; i++) {
+			int j = 1;
+			if (i % 2 == 1) {
+				j = 0;
+			}
+			for (; j < NUM_BOX_ROW; j += 2) {
+				canvas.drawBitmap(piece_black, matrix[i][j].x + correct
+						+ ((int) (pos_x / 2.0) + 2), matrix[i][j].y + correct,
+						null);
+			}
+			rows += 1;
+
+		}
+
+		// White pieces
+		for (int i = NUM_BOX_ROW - PLAYER_ROWS; i < NUM_BOX_ROW; i++) {
+			int j = 1;
+			if (i % 2 == 1) {
+				j = 0;
+			}
+			for (; j < NUM_BOX_ROW; j += 2) {
+				canvas.drawBitmap(piece_white, matrix[i][j].x
+						+ correct + ((int) (pos_x / 2.0) + 2), matrix[i][j].y
+						+ correct, null);
+			}
+		}
 	}
 
 	@Override
@@ -97,6 +124,12 @@ public class Board extends View {
 		board = Bitmap.createScaledBitmap(board, screen_size.x, new_y_board,
 				false);
 
+		board_center_x = (int) (1.0 * board.getWidth() / SIZE_BOARD_ORIGIN.x * CENTER_BOARD_ORIGIN.x);
+		board_center_y = (int) (1.0 * board.getHeight() / SIZE_BOARD_ORIGIN.y * CENTER_BOARD_ORIGIN.y);
+
+		box_size_x = (int) (1.0 * board.getWidth() / SIZE_BOARD_ORIGIN.x * SIZE_BOX_ORIGIN.x);
+		box_size_y = (int) (1.0 * board.getHeight() / SIZE_BOARD_ORIGIN.y * SIZE_BOX_ORIGIN.y);
+
 		// Load pieces images
 		piece_black = BitmapFactory.decodeResource(res,
 				R.drawable.piece_black_horizontal);
@@ -104,6 +137,9 @@ public class Board extends View {
 				R.drawable.piece_white_horizontal);
 
 		int new_x_piece = (int) (board.getWidth() / (NUM_BOX_ROW + 1.3));
+		if (new_x_piece >= box_size_x) {
+			new_x_piece = box_size_x - 5;
+		}
 		int new_y_piece = (int) (1.0 * piece_black.getHeight()
 				/ piece_black.getWidth() * new_x_piece);
 
@@ -112,12 +148,16 @@ public class Board extends View {
 		piece_white = Bitmap.createScaledBitmap(piece_white, new_x_piece,
 				new_y_piece, false);
 
-		board_center_x = (int) (1.0 * board.getWidth() / SIZE_BOARD_ORIGIN.x * CENTER_BOARD_ORIGIN.x);
-		board_center_y = (int) (1.0 * board.getHeight() / SIZE_BOARD_ORIGIN.y * CENTER_BOARD_ORIGIN.y);
-
-		box_size_x = (int) (1.0 * board.getWidth() / SIZE_BOARD_ORIGIN.x * SIZE_BOX_ORIGIN.x);
-		box_size_y = (int) (1.0 * board.getHeight() / SIZE_BOARD_ORIGIN.y * SIZE_BOX_ORIGIN.y);
-		Log.i("BOX", box_size_x +"-"+box_size_y);
-
+		// Calculating all the boxes of Damiera
+		int xbox = board_center_x - box_size_x * NUM_BOX_ROW / 2;
+		int ybox = board_center_y - box_size_y * NUM_BOX_ROW / 2;
+		for (int i = 0; i < NUM_BOX_ROW; i++) {
+			for (int j = 0; j < NUM_BOX_ROW; j++) {
+				matrix[i][j] = new Point(xbox, ybox);
+				xbox += box_size_x;
+			}
+			xbox = board_center_x - box_size_x * NUM_BOX_ROW / 2;
+			ybox += box_size_y;
+		}
 	}
 }
